461681636a21b0a769d161faac8c45b36294b031
Agadyr <kairambekovmadi@gmail.com>
2024-01-21 15:15:17 +0600

    made categories table and get categories from db

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,15 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
     public function index(){
-        return [
-            'all',
-            'k0otakbas',
-            'trucnsk',
-        ];
+        return Category::get();
     }
 }
