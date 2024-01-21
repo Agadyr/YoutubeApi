3f29a27c321ceed6ddd1882b88d5b9e0761d0d4f
Agadyr <kairambekovmadi@gmail.com>
2024-01-21 21:21:33 +0600

    full refactor CategoryVideoSeeder

--- database/seeders/CategoryVideoSeeder.php
+++ database/seeders/CategoryVideoSeeder.php
@@ -6,6 +6,7 @@ use App\Models\Category;
 use App\Models\Video;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
 
 class CategoryVideoSeeder extends Seeder
@@ -13,38 +14,30 @@ class CategoryVideoSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-//    public function run(): void
-//    {
-//
-//
-//        $categoryVideo = [];
-//
-//        foreach ($categories as $category) {
-//            $randomVideos = Arr::random($videos, mt_rand(1, count($videos)));
-//
-//            foreach ($randomVideos as $randomVideo) {
-//                $categoryVideo[] =
-//                    [
-//                        'category_id' => $category,
-//                        'video_id' => $randomVideo,
-//                    ];
-//            }
-//        }
-//
-//
-//    }
-    public function run(){
-        $categories = Category::pluck('id');
-        $videos = Video::pluck('id');
+    public function run()
+    {
+        $categoryIds = Category::pluck('id');
+        $videoIds = Video::pluck('id');
 
-        $categoryVideos = $categories->map(function (int $id) use($videos){
-            return [
-                'category_id'=>$id,
-                'video_id'=>$videos->random(),
-            ];
-        });
+        $categoryVideos = $categoryIds->flatMap(
+            fn(int $id) => $this->categoryVideos($id, $this->randomVideoIds($videoIds))
+        );
 
 
         DB::table('category_video')->insert($categoryVideos->all());
+    }
+
+    private function categoryVideos(int $category_id, Collection $videoIds): Collection
+    {
+
+        return $videoIds->map(fn(int $id) => [
+            'category_id' => $category_id,
+            'video_id' => $id
+        ]);
+    }
+
+    private function randomVideoIds(Collection $ids): Collection
+    {
+        return $ids->random(mt_rand(1, count($ids)));
     }
 }
