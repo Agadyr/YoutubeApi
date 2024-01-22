4b6aa7ab90f01ba5e799ee1945b2ce204c3664cf
Agadyr <kairambekovmadi@gmail.com>
2024-01-22 22:31:31 +0600

    Check nested relationships

--- app/Traits/WithRelationships.php
+++ app/Traits/WithRelationships.php
@@ -4,14 +4,23 @@ namespace App\Traits;
 
 use Illuminate\Support\Arr;
 
-trait WithRelationships{
+trait WithRelationships
+{
 
     public function scopeWithRelationships($query, array|string $relationships)
     {
         $validrelationships = collect($relationships)
-            ->map(fn(string $relationships):array => explode('.',$relationships))
-            ->filter(fn(array $relationships):bool => in_array($relationships[0],static::$relationships))
-            ->map(fn(array $relationships):string => implode('.',$relationships))
+            ->map(fn(string $relationships): array => explode('.', $relationships))
+            ->filter(function (array $relationships) {
+                return collect($relationships)->reduce(function ($model, $relationship) {
+                    if ($model && method_exists($model, $relationship) && in_array($relationship, $model::$relationships)) {
+                        return $model->$relationship()->getRelated();
+                    }
+
+                    return null;
+                }, new static);
+            })
+            ->map(fn(array $relationships): string => implode('.', $relationships))
             ->all();
 
         return $query->with($validrelationships);
