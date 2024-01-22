7ccd0ee197b39423e854b1c0da26109e4efe2d43
Agadyr <kairambekovmadi@gmail.com>
2024-01-22 19:34:45 +0600

    CategoryVideoSeeder

--- database/seeders/CategoryVideoSeeder.php
+++ database/seeders/CategoryVideoSeeder.php
@@ -16,28 +16,18 @@ class CategoryVideoSeeder extends Seeder
      */
     public function run()
     {
-        $categoryIds = Category::pluck('id');
-        $videoIds = Video::pluck('id');
 
-        $categoryVideos = $categoryIds->flatMap(
-            fn(int $id) => $this->categoryVideos($id, $this->randomVideoIds($videoIds))
+        $videos = Video::get();
+
+        Category::get()->flatMap(
+            fn(Category $category) => $category->videos()->saveMany($this->randomVideos($videos))
         );
 
 
-        DB::table('category_video')->insert($categoryVideos->all());
     }
 
-    private function categoryVideos(int $category_id, Collection $videoIds): Collection
+    private function randomVideos(Collection $videos): Collection
     {
-
-        return $videoIds->map(fn(int $id) => [
-            'category_id' => $category_id,
-            'video_id' => $id
-        ]);
-    }
-
-    private function randomVideoIds(Collection $ids): Collection
-    {
-        return $ids->random(mt_rand(1, count($ids)));
+        return $videos->random(mt_rand(1, $videos->count()));
     }
 }
