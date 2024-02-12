39f73cf8ce1efe92d5a2036f9f72ab7b591491a4
Agadyr <kairambekovmadi@gmail.com>
2024-02-13 01:20:53 +0600

    delete api tokens

--- app/Http/Controllers/PersonalAccessTokenController.php
+++ app/Http/Controllers/PersonalAccessTokenController.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
+use Laravel\Sanctum\PersonalAccessToken;
 
 class PersonalAccessTokenController extends Controller
 {
@@ -27,5 +28,10 @@ class PersonalAccessTokenController extends Controller
 
         return ['token' => $user->createToken($request->device_name)->plainTextToken];
 
+    }
+
+    public function delete(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -28,10 +28,12 @@ Route::get('/comments', [CommentController::class, 'index']);
 Route::get('/comments/{comment}', [CommentController::class, 'show']);
 
 
-Route::middleware('auth:sanctum')->group(function (){
+Route::middleware('auth:sanctum')->group(function () {
     Route::post('/comments', [CommentController::class, 'store']);
     Route::put('/comments/{comment}', [CommentController::class, 'update']);
     Route::delete('/comments/{comment}', [CommentController::class, 'delete']);
+
+    Route::delete('/personal-access-tokens', [PersonalAccessTokenController::class, 'delete']);
 });
 
 
