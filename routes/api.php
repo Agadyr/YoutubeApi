95d734cdc32550b48f70ed8f2cc1803109d523b6
Agadyr <kairambekovmadi@gmail.com>
2024-02-13 01:12:59 +0600

    Authenticate comment requests

--- routes/api.php
+++ routes/api.php
@@ -26,9 +26,13 @@ Route::get('/videos/{video}', [VideoController::class, 'show']);
 
 Route::get('/comments', [CommentController::class, 'index']);
 Route::get('/comments/{comment}', [CommentController::class, 'show']);
-Route::post('/comments', [CommentController::class, 'store'])->middleware('auth:sanctum');
-Route::put('/comments/{comment}', [CommentController::class, 'update'])->middleware('auth:sanctum');
-Route::delete('/comments/{comment}', [CommentController::class, 'delete'])->middleware('auth:sanctum');
+
+
+Route::middleware('auth:sanctum')->group(function (){
+    Route::post('/comments', [CommentController::class, 'store']);
+    Route::put('/comments/{comment}', [CommentController::class, 'update']);
+    Route::delete('/comments/{comment}', [CommentController::class, 'delete']);
+});
 
 
 Route::post('/personal-access-tokens', [PersonalAccessTokenController::class, 'store']);
